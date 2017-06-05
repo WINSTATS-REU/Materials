62a42ab13b1254738bd54f1045beb664540b54df
Malone <aq7839yd@winona.edu>
2017-06-05 08:54:13 -0500

    BASH Materials

---
+++ Data_Science_Principles/download.sh
@@ -0,0 +1,7 @@
+curl -O https://s3.amazonaws.com/tripdata/201601-citibike-tripdata.zip
+curl -O https://s3.amazonaws.com/tripdata/201603-citibike-tripdata.zip
+curl -O https://s3.amazonaws.com/tripdata/201605-citibike-tripdata.zip
+curl -O https://s3.amazonaws.com/tripdata/201607-citibike-tripdata.zip
+curl -O https://s3.amazonaws.com/tripdata/201609-citibike-tripdata.zip
+curl -O https://s3.amazonaws.com/tripdata/201611-citibike-tripdata.zip
+curl -O https://s3.amazonaws.com/tripdata/201612-citibike-tripdata.zip
